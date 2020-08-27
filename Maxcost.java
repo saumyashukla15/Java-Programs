b3ca1137697bf9be10393ec34df14a3ccf7f7b3e
Saumya Shukla <65821357+saumyashukla15@users.noreply.github.com>
2020-08-27 09:19:04 -0700

    Add files via upload

---
+++ Maxcost.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public static void main(string arg[]){
+    int t,budget,n;
+    
+    Scanner sc=new Scanner(System.in);
+    t=sc.nextInt();
+    n=sc.nextInt();
+    budget=sc.nextInt();
+    
+    
+
+    
+}
